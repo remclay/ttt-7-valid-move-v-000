b68915534cae7cc0ddaf453154bef9a19c62a982
Remmy Clay <remmy.lauren@gmail.com>
2017-11-06 09:41:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,7 @@
 def valid_move?(board, index)
   if !position_taken?(board, index)
     return true
-  elsif
-    index.between?(1, 9))
+  elsif index.between?(1, 9)
     return true
   elsif position_taken?(board, index)
     return false
